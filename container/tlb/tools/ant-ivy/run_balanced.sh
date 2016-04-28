9766e09f11df6a0a2723728546e017986b1bbad0
Colton <mccurdyc@allegheny.edu>
2016-04-27 22:27:02 -0400

    docker stuff

---
+++ container/tlb/tools/ant-ivy/run_balanced.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export TLB_BASE_URL='http://141.195.23.248:7019'
+export TLB_SERVER_PORT=7019
+export TLB_BALANCER_PORT=12345
+export TLB_JOB_NAME='ant-ivy'
+export TLB_TOTAL_PARTITIONS=2
+export TEST_TASK='ant test.balanced'
+export ALL_PARTITIONS_RAN_VERIFICATION_TASK='ant assert.all.partitions.executed'
+
+source ../recipe.sh
