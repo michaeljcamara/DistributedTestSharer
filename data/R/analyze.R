074d87e1e3584b48037b6b97a317aced41a39ec8
Colton <mccurdyc@allegheny.edu>
2016-05-01 21:10:01 -0400

    minor adjustments

--- data/R/analyze.R
+++ data/R/analyze.R
@@ -33,8 +33,9 @@ calculate_averages <- function(data) {
     l <- dplyr::distinct(dplyr::select(data, number_nodes))
     d <- data.frame("tool" = character(),
                     "number_nodes" = integer(),
-                    "average_time" = double())
-    names(d) <- c("tool", "number_nodes", "average_time")
+                    "average_time" = double(),
+                    "t" = character())
+    names(d) <- c("tool", "number_nodes", "average_time", "t")
 
     for (i in l[[1]]) {
     a <- dplyr::filter(data, i == number_nodes)
@@ -42,9 +43,10 @@ calculate_averages <- function(data) {
     tool <- dplyr::distinct(dplyr::select(a, tool))
     number_nodes <- i
     average_time <- mean(a$time)
+    t <- dplyr::distinct(dplyr::select(a,t))
 
-    b <- data.frame(tool, number_nodes, average_time)
-    names(b) <- c("tool", "number_nodes", "average_time")
+    b <- data.frame(tool, number_nodes, average_time, t)
+    names(b) <- c("tool", "number_nodes", "average_time", "t")
     d <- rbind(d, b)
 
     }
@@ -89,3 +91,44 @@ visualize_nodes_versus_time_hist <- function(data) {
   return(p)
 }
 
+#' FUNCTION: visualize_nodes_versus_time_both
+#'
+#' compare number of nodes versus time given a set of data
+#' for both tools: DTS and TLB
+
+visualize_nodes_versus_time_both <- function(data) {
+    p <- ggplot2::ggplot(data, ggplot2::aes(x = number_nodes, y = average_time, colour = t)) +
+    ggplot2::geom_line() +
+    ggplot2::scale_shape(guide = ggplot2::guide_legend(title = ""), solid = FALSE) +
+    ggplot2::theme_grey(base_size = 6) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 20)) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 20)) +
+    ggplot2::theme(legend.text=ggplot2::element_text(size=20)) +
+    ggplot2::ylab("Average Time (ms)") +
+    ggplot2::xlab("Nodes (count)") +
+    ggplot2::labs(title = "SchemaAnalyst Test Suite",
+                  colour = "Tool:") +
+    ggplot2::theme(title = ggplot2::element_text(size=20), legend.position = "top",
+                                      panel.background = ggplot2::element_blank())
+  return(p)
+}
+
+#' FUNCTION: visualize_nodes_versus_time_box_both
+#'
+#' compare number of nodes versus time given a set of data in a boxplot
+#' for comparing side-by-side tools
+
+visualize_nodes_versus_time_box_both <- function(data) {
+    p <- ggplot2::ggplot(data, ggplot2::aes(x = number_nodes, y = time, group = number_nodes, fill = t)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::scale_shape(guide = ggplot2::guide_legend(title = ""), solid = FALSE) +
+    ggplot2::theme_grey(base_size = 6) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 20)) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 20)) +
+    ggplot2::ylab("Time (ms)") +
+    ggplot2::xlab("Nodes (count)") +
+    ggplot2::labs(title = "SchemaAnalyst Test Suite") +
+    ggplot2::theme(title = ggplot2::element_text(size=20), legend.position = "top",
+                                      panel.background = ggplot2::element_blank())
+  return(p)
+}
